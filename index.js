58ae767d5c86d1aff78f73ba87a6d479b2e2d874
pranik chainani <pranikc@gmail.com>
2021-08-13 09:35:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const r = document.getElementById("main")
+
+r.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Pranik is the champion!"
+
+i
